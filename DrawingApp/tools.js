5880ab29f134eae9e3b09f77bbd31abed5011377
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-09-08 22:00:07 +0530

    updated

--- DrawingApp/tools.js
+++ DrawingApp/tools.js
@@ -2,21 +2,34 @@ let pencil = document.querySelector("#pencil");
 let eraser = document.querySelector("#eraser");
 
 let pencilOptions = document.querySelector("#pencil-options");
+let eraserOptions = document.querySelector("#eraser-options");
 
 let activeTool = "pencil";
+let pencilWidth = ctx.lineWidth;
+let eraserWidth = ctx.lineWidth;
 
 pencil.addEventListener("click", function(){
+    console.log("Pencil clicked!")
     activeTool = "pencil";
 
     if(pencil.classList.contains("active-tool")){
+        console.log("It contains active-tool class");
         if(pencilOptions.classList.contains("tool-options-active")){
+            console.log("Tools option are already active");
             pencilOptions.classList.remove("tool-options-active");
         }
         else {
+            console.log("Activate the tool options!")
             pencilOptions.classList.add("tool-options-active");
         }
 
     } else {
+        console.log("It does not active-tool class");
+        eraser.classList.remove("active-tool");
+        eraserOptions.classList.remove("tool-options-active");
+        pencil.classList.add("active-tool");
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = pencilWidth;
         
     }
 })

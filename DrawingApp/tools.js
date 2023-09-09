1fce56097a0583a424036374c87e40aef713ef61
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2023-09-09 19:45:59 +0530

    slider set

--- DrawingApp/tools.js
+++ DrawingApp/tools.js
@@ -4,6 +4,16 @@ let eraser = document.querySelector("#eraser");
 let pencilOptions = document.querySelector("#pencil-options");
 let eraserOptions = document.querySelector("#eraser-options");
 
+let black = document.querySelector(".black");
+let red = document.querySelector(".red");
+let yellow = document.querySelector(".yellow");
+let blue = document.querySelector(".blue");
+
+
+let pencilSlider = document.querySelector("#pencil-slider");
+let eraserSlider = document.querySelector("#eraser-slider");
+
+
 let activeTool = "pencil";
 let pencilWidth = ctx.lineWidth;
 let eraserWidth = ctx.lineWidth;
@@ -32,4 +42,47 @@ pencil.addEventListener("click", function(){
         ctx.lineWidth = pencilWidth;
         
     }
+});
+
+eraser.addEventListener("click", function(){
+    activeTool = "eraser";
+    if(eraser.classList.contains("active-tool")){
+        if(eraserOptions.classList.contains("tool-options-active")){
+            eraserOptions.classList.remove("tool-options-active");
+        } else {
+            eraserOptions.classList.add("tool-options-active");
+        }
+    } else {
+        pencil.classList.remove("active-tool");
+        pencilOptions.classList.remove("tool-options-active");
+        eraser.classList.add("active-tool");
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = eraserWidth;
+    }
+});
+
+black.addEventListener("click", function(){
+    ctx.strokeStyle = "black";
+});
+
+blue.addEventListener("click", function(){
+    ctx.strokeStyle = "blue";
+});
+
+yellow.addEventListener("click", function(){
+    ctx.strokeStyle = "yellow";
+});
+
+red.addEventListener("click", function(){
+    ctx.strokeStyle = "red";
+});
+
+pencilSlider.addEventListener("change", function(){
+    pencilWidth = pencilSlider.value;
+    ctx.lineWidth = pencilWidth;
+})
+
+eraserSlider.addEventListener("change", function(){
+    eraserWidth = eraserSlider.value;
+    ctx.lineWidth = eraserWidth;
 })
